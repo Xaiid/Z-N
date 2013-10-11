d83506f54c841777e6f6dc12211b2dc5e740a3c0
Narciso Guillen <narciso.guillen@tangosource.com>
2013-10-11 12:00:00 -0500

    Start main script for game [FRONT]

---
+++ public/javascripts/app.js
@@ -0,0 +1,15 @@
+var ZombieWorld = {
+
+  Land: {},
+  socket: null,
+  Components: {},
+  Entities: {},
+  currentPlayer: null,
+  Players: {},
+  Zombies: {},
+  Scenes: {},
+
+  Initialize: function(){
+  }
+
+};
