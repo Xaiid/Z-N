ee93ee64a04872ec9b103a18552d226a2f940dc6
Jeaneth <xaid27@gmail.com>
2013-10-11 19:29:11 -0500

    removed color from zombie

--- public/javascripts/app/components/zombie.js
+++ public/javascripts/app/components/zombie.js
@@ -1,7 +1,6 @@
 ZombieWorld.Components.actor = Crafty.c('Zombie', {
   init: function(){
-    this.addComponent('Actor, Solid, Mouse, Color')
-    .color('rgb(112,123,123)') 
+    this.addComponent('Actor, Solid, Mouse')
     .bind('Click', function(e) {
       console.log("You clicked a zombie")
     });
