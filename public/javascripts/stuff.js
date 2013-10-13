b08816af42a9f802633dce7afe24a0679bf69d55
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-10-12 22:05:46 -0500

    Select character sounds

--- public/javascripts/stuff.js
+++ public/javascripts/stuff.js
@@ -5,5 +5,18 @@ $(function(){
     $('.player').on('click',function(){
         var av = $(this).data('player')
         $("#avatar-player li img").attr('src','/images/'+av+'.png');
+        switch(av.split('_')[1]){
+          case '1':
+            dir = 'gun';
+            break;
+          case '2':
+            dir = 'shotgun';
+            break;
+          case '3':
+            dir = 'rocket';
+            break;
+        }
+        var clickSound = new Audio('/sounds/'+dir+'/select.mp3');
+        clickSound.play();
     });
 });
